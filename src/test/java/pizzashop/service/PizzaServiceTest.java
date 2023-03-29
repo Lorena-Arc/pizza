1c4cf6bd0708fcc6d2d26c571bde0cd3867f1e03
Ioana0723 <74377457+Ioana0723@users.noreply.github.com>
2023-03-29 11:54:55 +0300

    teste wbt

--- src/test/java/pizzashop/service/PizzaServiceTest.java
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -12,6 +12,8 @@ import pizzashop.model.PaymentType;
 import pizzashop.repository.MenuRepository;
 import pizzashop.repository.PaymentRepository;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -63,7 +65,7 @@ class PizzaServiceTest {
     @Test
     @Order(2)
     @DisplayName("Test for invalid table number - ECP")
-    void addPaymentWithInvalidTableNoECP(){
+    void addPaymentWithInvalidTableNoECP() {
         // given
         Payment payment = new Payment(11, PaymentType.Cash, 230d);
         System.out.println(payment.getTableNumber());
@@ -73,13 +75,13 @@ class PizzaServiceTest {
 
         // then
         verifyNoInteractions(paymentRepository);
-        assertFalse(payment.getTableNumber()>0 && payment.getTableNumber()<9);
+        assertFalse(payment.getTableNumber() > 0 && payment.getTableNumber() < 9);
     }
 
     @Test
     @Order(3)
     @DisplayName("Test for invalid amount number - ECP")
-    void addPaymentWithInvalidAmountECP(){
+    void addPaymentWithInvalidAmountECP() {
         // given
         Payment payment = new Payment(5, PaymentType.Cash, -60d);
         System.out.println(payment.getTableNumber());
@@ -89,7 +91,7 @@ class PizzaServiceTest {
 
         // then
         verifyNoInteractions(paymentRepository);
-        assertFalse(payment.getAmount()>0);
+        assertFalse(payment.getAmount() > 0);
     }
 
 
@@ -97,7 +99,7 @@ class PizzaServiceTest {
     @Order(4)
     @ValueSource(ints = {1, 8})
     @DisplayName("Test for valid table number - BVA")
-    void tableNumberRange(int i){
+    void tableNumberRange(int i) {
         // given
         Payment payment = new Payment(i, PaymentType.Card, 0.01);
 
@@ -109,10 +111,11 @@ class PizzaServiceTest {
         verifyNoMoreInteractions(paymentRepository);
         assertTrue(i >= 1 && i <= 8);
     }
+
     @Test
     @Order(5)
     @DisplayName("Test for 0 table number - BVA")
-    void addPaymentWith0TableNo(){
+    void addPaymentWith0TableNo() {
         // given
         Payment payment1 = new Payment(0, PaymentType.Cash, 19.3);
         System.out.println(payment1.getTableNumber());
@@ -122,13 +125,13 @@ class PizzaServiceTest {
 
         // then
         verifyNoInteractions(paymentRepository);
-        assertFalse(payment1.getTableNumber()>0 && payment1.getTableNumber()<9);
+        assertFalse(payment1.getTableNumber() > 0 && payment1.getTableNumber() < 9);
     }
 
     @Test
     @Order(6)
     @DisplayName("Test for 9 table number - BVA")
-    void addPaymentWith9TableNo(){
+    void addPaymentWith9TableNo() {
         // given
         Payment payment = new Payment(9, PaymentType.Card, 0.01);
         System.out.println(payment.getTableNumber());
@@ -138,14 +141,14 @@ class PizzaServiceTest {
 
         // then
         verifyNoInteractions(paymentRepository);
-        assertFalse(payment.getTableNumber()>0 && payment.getTableNumber()<9);
+        assertFalse(payment.getTableNumber() > 0 && payment.getTableNumber() < 9);
     }
 
     @Test
     @Order(7)
     @Timeout(1)
     @DisplayName("Test for zero amount - BVA")
-    void addPaymentWithZeroAmount(){
+    void addPaymentWithZeroAmount() {
         // given
         Payment payment = new Payment(7, PaymentType.Cash, 0.0);
 
@@ -161,7 +164,7 @@ class PizzaServiceTest {
     @Order(8)
     @Timeout(1)
     @DisplayName("Test for negative amount - BVA")
-    void addPaymentWithInvalidAmount(){
+    void addPaymentWithInvalidAmount() {
         // given
         Payment payment1 = new Payment(7, PaymentType.Cash, -0.1);
 
@@ -177,7 +180,7 @@ class PizzaServiceTest {
     @Order(9)
     @Timeout(1)
     @DisplayName("Test for positive amount - BVA")
-    void addPaymentWithSomePositiveAmount(){
+    void addPaymentWithSomePositiveAmount() {
         // given
         Payment payment1 = new Payment(7, PaymentType.Cash, 0.01);
 
@@ -189,4 +192,83 @@ class PizzaServiceTest {
         verifyNoMoreInteractions(paymentRepository);
         assertFalse(payment1.getAmount() < 0);
     }
+
+
+    //lab03 tests
+    @Test
+    @DisplayName("Test valid WBT")
+    void getTotalAmount0() {
+        //given
+        Double totalCash, totalCard;
+
+        //when
+        totalCash = pizzaService.getTotalAmount(PaymentType.Cash);
+        totalCard = pizzaService.getTotalAmount(PaymentType.Card);
+
+        //then
+        assertEquals(0, totalCash);
+        assertEquals(0, totalCard);
+    }
+
+    @Test
+    @DisplayName("Test invalid WBT")
+    void getTotalAmountSuccessfully() {
+        //given
+        Double totalCash = 0.0d, totalCard = 0.0d;
+        Payment payment1 = new Payment(7, PaymentType.Cash, 0.01);
+        Payment payment2 = new Payment(1, PaymentType.Cash, 5.01);
+        Payment payment3 = new Payment(1, PaymentType.Cash, 5.01);
+        Payment payment4 = new Payment(5, PaymentType.Card, 23.3);
+        Payment payment5 = new Payment(8, PaymentType.Card, 14.5);
+
+        //when
+        List<Payment> payments = pizzaService.getPayments();
+        payments.add(payment1);
+        payments.add(payment2);
+        payments.add(payment3);
+        payments.add(payment4);
+        payments.add(payment5);
+        when(paymentRepository.getAll()).thenReturn(payments);
+
+        totalCash = pizzaService.getTotalAmount(PaymentType.Cash);
+        totalCard = pizzaService.getTotalAmount(PaymentType.Cash);
+
+        //then
+        assertEquals(totalCash, 10.03);
+        assertTrue(totalCard > 0);
+    }
+
+    @Test
+    @DisplayName("Test invalid WBT")
+    void getTotalAmount00() {
+        //given
+        Double totalCash, totalCard;
+
+        //when
+        totalCash = pizzaService.getTotalAmount(null);
+        totalCard = pizzaService.getTotalAmount(null);
+        System.out.println(totalCard);
+        //then
+        assertEquals(totalCash, 0.0);
+        assertEquals(totalCard, 0.0);
+    }
+
+    @Test
+    @DisplayName("Test valid WBT")
+    void getTotalAmountNull() {
+        //given
+        Double totalCash, totalCard;
+        List<Payment> payments = null;
+        when(paymentRepository.getAll()).thenReturn(payments);
+
+        //when
+        totalCash = pizzaService.getTotalAmount(PaymentType.Cash);
+        totalCard = pizzaService.getTotalAmount(PaymentType.Card);
+
+        //then
+        assertNull(payments);
+        assertEquals(0.0, totalCard);
+        assertEquals(0.0, totalCash);
+    }
+
 }
